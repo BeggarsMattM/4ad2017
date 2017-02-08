ccbba6c682f13d9c97eaa50c6f7f36b2454c79ab
Matthew Marcus <matthewmarcus@beggars.com>
2017-02-08 17:42:01 +0000

    Fixed Laravel 5.4 migration error issue

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $user = factory(User::class)->create([
+          'email' => 'testuser@beggars.com',
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->assertPathIs('/home');
+        });
+    }
+}
